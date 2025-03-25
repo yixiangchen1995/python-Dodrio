596186c4cf88e655aa4dc0fb6da4cf0f047029c6
yixiangchen1995 <yixiangchen1995@gmail.com>
2025-03-25 14:58:35 +0800

    Add gen_package function

--- dodrio/__init__.py
+++ dodrio/__init__.py
@@ -1,3 +1,12 @@
+'''
+FilePath: /python-Dodrio/dodrio/__init__.py
+Descripttion: 
+Author: Yixiang Chen
+version: 
+Date: 2025-03-24 15:50:51
+LastEditors: Yixiang Chen
+LastEditTime: 2025-03-25 14:48:08
+'''
 
 
 from dodrio.core.parquet_base import (
@@ -5,7 +14,7 @@ from dodrio.core.parquet_base import (
 )
 
 from dodrio.core.package_base import (
-    parquet2package, package2wav
+    parquet2package, package2wav, gen_package
 )
 
 from dodrio.core.datainfo_process import gen_infodir
--- dodrio/core/package_base.py
+++ dodrio/core/package_base.py
@@ -6,9 +6,11 @@ from tqdm import tqdm
 import glob
 from io import BytesIO
 import json
+import math
 from scipy.io import wavfile
 import pyarrow.parquet as pq
 
+from dodrio.core.utils import set_wavlist
 
 ############## Parquet to Package ###############   
 
@@ -32,7 +34,7 @@ def audio_regular(audio, ori_sample_rate, dtype, target_sample_rate=48000):
     int16_audio = (rs_audio*32768).astype(np.int16)
     return int16_audio
 
-def load_mp3(bio, target_sample_rate=48000):
+def load_mp3_frombio(bio, target_sample_rate=48000):
     audio, ori_sr = librosa.load(BytesIO(bio))
     rs_audio = librosa.resample(audio, orig_sr=ori_sr, target_sr=target_sample_rate)
     int16_audio = (rs_audio*32768).astype(np.int16)
@@ -55,6 +57,7 @@ def parquet2package_single(parquet_file, package_file, target_sample_rate=48000)
     info_list = []
     audio_pos = {}
     outf = open(package_file, 'wb')
+    num_utts_per_parquet = len(df)
     for idx in tqdm(range(len(df)), desc=f'{basename} Processing'):
         utt = df.iloc[idx]['utt']
         sr = df.iloc[idx]['sample_rate']
@@ -63,7 +66,7 @@ def parquet2package_single(parquet_file, package_file, target_sample_rate=48000)
         if ftype == 'wav':
             reg_audio = audio_regular(audio, sr, dtype, target_sample_rate)
         elif ftype == 'mp3':
-            reg_audio = load_mp3(audio, target_sample_rate) 
+            reg_audio = load_mp3_frombio(audio, target_sample_rate) 
         else:
             print("Now just accept mp3 and wav format")
             return
@@ -77,21 +80,101 @@ def parquet2package_single(parquet_file, package_file, target_sample_rate=48000)
         info_list.append([utt, os.path.split(package_file)[-1], str(position), str(end_position)])
 
         position += byte_num
-    return info_list
+    return info_list, num_utts_per_parquet
 
 def parquet2package(parquet_dir, package_dir, sample_rate=48000):
     os.makedirs(package_dir, exist_ok=True)
     pq_file_list = glob.glob(parquet_dir+'/*.parquet')
     pq_file_list.sort()
     all_info_list = []
-    num_utts_per_parquet = len(pq_file_list)
-    save_meta_info(package_dir, sample_rate, num_utts_per_parquet)
+    sflag = True
     for one_pq in pq_file_list:
         basename = os.path.split(one_pq)[-1].split('.parquet')[0]
         ftype = os.path.split(one_pq)[-1].split('_')[0]
         pack_file = os.path.join(package_dir, basename+'.pack')
-        info_list = parquet2package_single(one_pq, pack_file, sample_rate)
+        info_list, num_utts_per_parquet = parquet2package_single(one_pq, pack_file, sample_rate)
+        if sflag:
+            save_meta_info(package_dir, sample_rate, num_utts_per_parquet)
+            sflag = False
         all_info_list.append(info_list)
+    info_outfile = os.path.join(package_dir, 'uttinfo.list')
+    with open(info_outfile, 'w') as outf:
+        for info_list in all_info_list:
+            for info in info_list:
+                outline = '|'.join(info) + '\n'
+                outf.write(outline)
+
+############## Wav to Package ############### 
+
+def load_mp3(mp3path, target_sample_rate=48000):
+    audio, ori_sr = librosa.load(mp3path)
+    rs_audio = librosa.resample(audio, orig_sr=ori_sr, target_sr=target_sample_rate)
+    int16_audio = (rs_audio*32768).astype(np.int16)
+    return int16_audio
+
+def load_audio_content(utt, audiopath, target_sample_rate, file_type='wav'):
+    if file_type == 'wav':
+        sr, audio = wavfile.read(audiopath)
+        if len(audio)<1:
+            print(f"{utt} wavfile is None")
+            return None
+        if len(audio.shape) > 1:
+            print(f"{utt} {str(len(audio.shape))} channel is non-mono channel, just first channel will be save")
+            audio = audio[:,0]
+        dtype = str(audio.dtype)
+        int16_audio = audio_regular(audio, sr, dtype, target_sample_rate)
+    elif file_type == 'mp3':
+        int16_audio = load_mp3(audiopath, target_sample_rate) 
+    else:
+        print("Now just accept mp3 and wav format")
+        return None
+    return int16_audio 
+
+def gen_package(wav_dir, package_dir, mid_name='', target_sample_rate=48000, file_type='wav', num_utts_per_parquet=2000, process_max_num=10000):
+    os.makedirs(package_dir, exist_ok=True)
+    wavdict, uttlist = set_wavlist(wav_dir, file_type)
+
+    pack2utt = {}
+    turn_num = math.ceil(len(uttlist) / process_max_num)
+    all_info_list = []
+    true_len = min(len(uttlist), num_utts_per_parquet)
+    save_meta_info(package_dir, target_sample_rate, true_len)
+    for tid in range(turn_num):
+        wavinfo_dict ={}
+        for utt in tqdm(uttlist[process_max_num * tid : process_max_num * (tid+1)], desc=f'Turn{str(tid)}LoadAudio'):
+            wavpath = wavdict[utt]
+            int16_audio = load_audio_content(utt, wavpath, target_sample_rate)
+            wavinfo_dict[utt] = int16_audio
+
+        prefix = file_type
+        pack_list = []
+        for i, j in enumerate(range(process_max_num * tid, min(len(uttlist), process_max_num * (tid+1)), num_utts_per_parquet)):
+            idx = (process_max_num * tid) // num_utts_per_parquet + i
+            pfile = prefix + '_' + mid_name + '_{:05d}.pack'.format(idx)
+            pack_file = os.path.join(package_dir, pfile)
+            pack_list.append(pack_file)
+            pack2utt[pfile] = uttlist[j: j + num_utts_per_parquet] 
+
+            position = 0
+            info_list = []
+            audio_pos = {}
+            outf = open(pack_file, 'wb')
+            for idx in tqdm(range(len(uttlist[j: j + num_utts_per_parquet])), desc=f'{pfile} Processing'):
+                utt = uttlist[j+idx]
+                int16_audio = wavinfo_dict[utt]
+                if int16_audio is None:
+                    continue
+                byte_audio = bytes(int16_audio)
+                outf.write(byte_audio)
+
+                byte_num = len(int16_audio)* 2 
+                end_position = position+byte_num 
+                audio_pos[utt] = [position, end_position]
+                info_list.append([utt, os.path.split(pack_file)[-1], str(position), str(end_position)])
+
+                position += byte_num
+            outf.close()
+            all_info_list.append(info_list)
     info_outfile = os.path.join(package_dir, 'uttinfo.list')
     with open(info_outfile, 'w') as outf:
         for info_list in all_info_list:
